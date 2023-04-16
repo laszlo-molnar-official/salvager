a37055a4a6a0d9e0ed4c82b7d64207626c34835c
SX <sxcorp@gmail.com>
2023-04-16 13:07:54 +0200

    target boat prefabs with basic LevelTarget script

---
+++ Salvager/Assets/_Salvager/Scripts/Level/LevelTarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets._Salvager.Scripts.Level
+{
+    [Serializable]
+    public class LevelTarget : MonoBehaviour
+    {
+        [SerializeField, Tooltip("ID is used to identify this type of target. " +
+            "Set it in the prefab.")]
+        private int id = -1;
+
+        public int ID { get { return id; } private set { id = value; } }
+    }
+}
