3c28a28c8bd8166fcaa08b96accda4a949cc8c75
SX <sxcorp@gmail.com>
2023-04-17 10:44:57 +0200

    menu show on start, will move to a separate scene

---
+++ Salvager/Assets/_Salvager/Scripts/Globals/Utils/InputActionMapChanger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Globals.Utils
+{
+    public class InputActionMapChanger : MonoBehaviour
+    {
+        public void ChangeMap(string mapName)
+        {
+            PlayerInput.GetPlayerByIndex(0).SwitchCurrentActionMap(mapName);            
+        }
+    }
+}
